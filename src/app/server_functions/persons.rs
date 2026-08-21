17cc93aceef864ede9a643a30b22270bf7ecb4b3
mac <@b.tunde.ipaye@gmail.com>
2026-08-21 13:43:59 +0100

    refractor: server function person updated commit by ENGR. IPAYE

--- src/app/server_functions/persons.rs
+++ src/app/server_functions/persons.rs
@@ -7,6 +7,20 @@ pub async fn get_persons() -> Result<Vec<Person>, ServerFnError>{
 
     let persons = retrieve_all_persons().await;
     Ok(persons)
+    }
 
+cfg_if::cfg_if!{
 
+    if #[cfg(feature = "ssr")]{
+
+        use crate::app::db::database;
+
+        pub async fn retrieve_all_persons() -> Vec<Person>{
+
+            let get_all_persons_result = database::get_all_persons().await;
+            match get_all_persons_result{
+                Some(found_persons)
+                }
+            }
+        }
     }
