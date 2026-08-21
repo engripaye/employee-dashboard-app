c0295707abdee03fa86f0530d61edb04d4337290
mac <@b.tunde.ipaye@gmail.com>
2026-08-21 12:00:17 +0100

    feat: person models updated commit by ENGR. IPAYE

--- src/app/models/person.rs
+++ src/app/models/person.rs
@@ -5,5 +5,16 @@ use validator::Validate;
 pub struct Person {
 
     pub uuid: String,
-
+    #[validate(length(min = 1, message = "name is required"))]
+    pub name: String,
+    #[validate(length(min = 1, message = "title is required"))]
+    pub title: String,
+    #[validate(length(min = 1, message = "level is required"))]
+    pub level: String,
+    #[validate(range(min = 2000, max = 99999))]
+    pub compensation: i32,
+    pub joined_date: String,
     }
+
+    impl_person {
+        }
