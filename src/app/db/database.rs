12d43abdcc45e8cfa8b01884c5933a959d944f32
mac <@b.tunde.ipaye@gmail.com>
2026-08-23 17:36:23 +0100

    fix: database attribute typo updated commit by ENGR. IPAYE

--- src/app/db/database.rs
+++ src/app/db/database.rs
@@ -1,6 +1,6 @@
 cfg_if::cfg_if! {
 
-    if #[cfg(feature - "ssr")]{
+    if #[cfg(feature = "ssr")]{
         use create::app::models::Person;
         use surrealdb::engine::remote::ws::{Client, Ws};
         use surrealdb::opt::auth::Root;
